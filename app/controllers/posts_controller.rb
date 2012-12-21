238d9fd129ad61a9499549dba0508cdcc60a4a09
Dara Thanh Nam Cam <dnam86@gmail.com>
2012-12-21 01:45:07 -0500

    Added post form and new comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,18 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to root_path, notice: "Post created successfully"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CourseProject::Application.routes.draw do
   root to: "posts#index"
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:create]
+  end
 end
